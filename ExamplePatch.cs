1fe8b6595d75dd4d9b2d8a941f4c9fbb3faa9516
NTkernel <ntkernel1@gmail.com>
2024-08-25 15:53:24 +0100

    Add files via upload

---
+++ ExamplePatch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This is an example patch, made to demonstrate how to use Harmony. You should remove it if it is not used.
+	/// </summary>
+	
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomGrabbableMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.NTkernel.gorillatag.Blahaj";
+        public const string Name = "Blahaj";
+        public const string Version = "1.0.1";
+    }
+}
